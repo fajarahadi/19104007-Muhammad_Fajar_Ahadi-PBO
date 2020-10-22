c269ac1435e4c6a19918101a747d6bd9dbd24397
fajarahadi <fajarahadi16@gmail.com>
2020-10-22 11:01:53 +0700

    Praktikum & Tugas Pertemuan 1

--- PraktikumPBO/src/id/fajarahadi/pertemuan1/Praktikum/Class/Kelas.java
+++ PraktikumPBO/src/id/fajarahadi/pertemuan1/Praktikum/Class/Kelas.java
@@ -1,4 +1,15 @@
 package id.fajarahadi.pertemuan1.Praktikum.Class;
 
 public class Kelas {
+    private static String nama = "Paimon";
+
+    private static int setMinute(){
+        int minute = 10;
+        return minute;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nama\t\t\t: " + nama);
+        System.out.println("Set Minute\t\t: " + setMinute());
+    }
 }
